c406b6b67a0e28832581bf9ae10d4242254d2235
Meghna Mrinal <meghna.mrinal.3@gmail.com>
2026-07-31 12:31:16 +0530

    Added styling

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let but = document.querySelectorAll(".button");
-but.onclick = () => {
-    console.log("C");
-};
+let reset = document.querySelector("reset");
+
+let turn0 = true;
+
+let win_pat = [[0,1,2], [3,4,5], [6,7,8],
+               [0,3,6], [1,4,7], [2,5,8],
+               [0,4,8], [6,4,2]];
